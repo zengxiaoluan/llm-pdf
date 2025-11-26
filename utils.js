916acc59cb6c519188173dce101f6f18d424d1dd
zengxiaoluan <370203400@qq.com>
2025-11-26 23:39:22 +0800

    纯前端的方式调用 deepseek

--- utils.js
+++ utils.js
@@ -8,9 +8,12 @@ const openai = new OpenAI({
 });
 
 export async function chatWithDeepseek(content) {
+  let modelA = "deepseek-reasoner";
+  let modelB = "deepseek-chat";
+
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content }],
-    model: "deepseek-chat",
+    model: modelB,
   });
 
   return completion.choices[0].message.content;
